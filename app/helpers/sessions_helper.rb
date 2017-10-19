c6335269ea120154fd201b91fa2d8641b1c3b2b0
Kylie <khanhlinh970@gmail.com>
2017-10-19 14:36:43 +0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,11 @@ module SessionsHelper
     	session[:user_id] = user.id
   	end
 
-  	# Returns the current logged-in user (if any).
-  	# def current_user
-   #  	@current_user ||= User.find_by(id: session[:user_id])
-  	# end
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user == current_user
+    end
 
   	# Returns the user corresponding to the remember token cookie.
     def current_user
@@ -47,11 +48,6 @@ module SessionsHelper
   	def logged_in?
     	!current_user.nil?
   	end
-
-    # Returns true if the given user is the current user.
-    def current_user?(user)
-      user == current_user
-    end
 
     # Redirects to stored location (or to the default).
     def redirect_back_or(default)
